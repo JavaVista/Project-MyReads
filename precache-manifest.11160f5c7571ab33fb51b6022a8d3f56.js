9d432e8cf05786aff69feb7c85a60fac9f9dc739
Javi Carrion <suavejavi@google.com>
2019-01-08 12:47:52 -0500

    Updates

---
+++ precache-manifest.11160f5c7571ab33fb51b6022a8d3f56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/Project-MyReads/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/Project-MyReads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/Project-MyReads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "8ce6013fcae40eb838f5",
+    "url": "/Project-MyReads/static/js/runtime~main.8ce6013f.js"
+  },
+  {
+    "revision": "72f35ceac6a0bcab65e1",
+    "url": "/Project-MyReads/static/js/main.72f35cea.chunk.js"
+  },
+  {
+    "revision": "f567cd14846ca13d3e3d",
+    "url": "/Project-MyReads/static/js/1.f567cd14.chunk.js"
+  },
+  {
+    "revision": "72f35ceac6a0bcab65e1",
+    "url": "/Project-MyReads/static/css/main.9a482fb9.chunk.css"
+  },
+  {
+    "revision": "7304109900c8578f8ce199bf0c1820b7",
+    "url": "/Project-MyReads/index.html"
+  }
+];
